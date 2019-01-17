a6d0330d56b7b55e68ef66cb14f825d30e916951
Delacqua, Francesco <francesco.delacqua@vumc.org>
2019-01-17 16:17:15 -0600

    cleanup

--- EpicParticipantUpdater.php
+++ EpicParticipantUpdater.php
@@ -4,7 +4,6 @@ namespace Vanderbilt\EpicParticipantUpdater;
 require_once join(['vendor','autoload.php'],DIRECTORY_SEPARATOR);
 
 use Vanderbilt\EpicParticipantUpdater\App\Helpers\File as FileHelper;
-use Vanderbilt\EpicParticipantUpdater\App\Helpers\Logger;
 use Vanderbilt\EpicParticipantUpdater\App\Helpers\EpicXMLParser;
 // use Vanderbilt\EpicParticipantUpdater\App\Helpers\DependencyHelper;
 
@@ -16,29 +15,5 @@ class EpicParticipantUpdater extends AbstractExternalModule {
     {
         parent::__construct();
     }
-
-    function redcap_module_system_enable($version)
-    {
-        Logger::log($this->logFile, "enabled");
-    }
-    
-    function redcap_module_system_disable($version)
-    {
-        Logger::log($this->logFile, "disabled");
-    }
-
-    /* public function log($message,$parameters=[])
-    {
-        foreach ($parameters as $key => &$value) {
-            if($key==='message')
-            {
-                // message cannot be a parameter
-                unset($parameters[$key]);
-            }else {
-                if(is_array($value)) $value = implode(', ',$value);
-            }
-        }
-        parent::log($message,$parameters);
-    } */
 
 }
