13b58feecded772e321bfd5ec56fded47fc7b9bb
Delacqua, Francesco <francesco.delacqua@vumc.org>
2019-03-14 11:50:00 -0500

    show version of the module in the dashboard

--- header.php
+++ header.php
@@ -1,10 +1,17 @@
 
+<?php
+function printVersion($module)
+{
+  $version = $module->VERSION ? sprintf("(%s)", $module->VERSION) : '';
+  echo $version;
+}
+?>
 
 <link rel="stylesheet" type="text/css" href="<?= $module->getUrl('./assets/css/style.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?= $module->getUrl('./assets/css/fontawesome/css/all.css'); ?>">
   
 <nav id="epu_menu" class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Epic Participant Updater</a>
+  <a class="navbar-brand" href="#">Epic Participant Updater <?php printVersion($module)?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
